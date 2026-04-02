80d52fc182d3729644ee93a6970de3fbaebfefb4
Subham <de.subham1@gmail.com>
2026-04-02 11:21:15 +0200

    Contact model created

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from clients.models import Client
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    clients = models.ManyToManyField(Client, blank=True, related_name='contacts')
+
+    class Meta:
+        ordering = ['surname', 'name']
+        indexes = [
+            models.Index(fields=['surname', 'name']),
+        ]
+
+    def __str__(self):
+        return f'{self.surname} {self.name}'
+
+    @property
+    def full_name(self):
+        return f'{self.surname} {self.name}'
